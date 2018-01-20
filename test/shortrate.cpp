88e91bc4aa6c684471255fada0850c7d6c83e1d4
Roy Zywina <roy@zywina.ca>
2018-01-19 22:57:48 -0500

    short rate model working

--- test/shortrate.cpp
+++ test/shortrate.cpp
@@ -1,9 +1,9 @@
 /*
 A minimal implementation of the FFT short rate model described in
-my paper (get link). Uses RFFT for small speed boost.
+my paper (https://ssrn.com/abstract=2984393
 
-This implementation concerns the pricing of a callable bond and
-can be used as a base for more complex secutities.
+This implementation concerns the pricing of a callable bond and). Uses RFFT
+for small speed boost.
 
 Requires QuantLib & boost.
 
@@ -13,7 +13,7 @@ Roy Zywina, (c) 2017, MIT licence (https://opensource.org/licenses/MIT)
 #include <cfftpack/cfftpack.h>
 #include <cfftpack/cfftextra.h>
 
-
+#include <cstdio>
 #include <vector>
 #include <cmath>
 #include <iostream>
@@ -35,11 +35,13 @@ typedef function<complex<double> (double u,double dt)> CharacteristcFunction;
 // conversion of Levy process to short rate function
 typedef function<double(double x,double gamma)> ShortRateConv;
 
+// a time step on thr lattice (I call it a mesh)
 class Step{
 public:
   double term, dt, bond, gamma;
   vector<complex<double>> phi;
   vector<double> x, u, r, fdf, ad, p;
+  vector<double> value;
   bool canExercise;
   double cashFlow, accrued;
   Step(){
@@ -55,17 +57,26 @@ public:
     fdf.resize(N,0);
     ad.resize(N,0);
     p.resize(N,0);
+    value.resize(N,0);
+  }
+
+  void printDebug(int i){
+    printf("step %3d, t %6.3f, df %.4f, gamma %.5f, CF %.2f, AI %.4f\n",
+      i, term, bond, gamma, cashFlow, accrued);
   }
 };
 
+// short rate model lattice
 class Mesh{
 public:
   fft_t *pfft;
   TimeGrid times;
   function<complex<double> (double u,double dt)> charFunc;
-  function<double (double x, double gamma)> covFunc;
+  // ength of real an complex arrays
   int N, NC;
+  // time step objects
   vector<Step> step;
+  CharacteristcFunction phi;
 
   Mesh(int Nfft, const TimeGrid &tg){
     times = tg;
@@ -82,7 +93,7 @@ public:
   }
 
   // finite difference estimate of stddev over lifetime of product
-  double estimsteSigma(CharacteristcFunction phi){
+  double estimsteSigma(){
     complex<double> fu,fm,fd;
     double h=0.1;
     double maxTerm = times.back();
@@ -103,20 +114,26 @@ public:
   // initialize arrays
   void initialize(
     double meanRev,
-    CharacteristcFunction phi)
+    CharacteristcFunction phi_)
   {
-    double sigma = estimsteSigma(phi);
-    double L = 2*10*sigma;
+    phi = phi_;
+    double sigma = estimsteSigma();
+    double maxTerm = times.back();
+    double L = 2*10*sigma * exp(meanRev*maxTerm) ;
     double dxm = L/N;
     double dum = 2.0*pi<double>()/(dxm*N);
     int n = N/2;
     for (size_t i=0; i<step.size(); i++){
       Step &s = step[i];
       s.term = times[i];
-      s.dt = times.dt(i);
+      if (i<step.size()-1)
+        s.dt = times[i+1] - times[i];
+      else
+        s.dt = step[i-1].dt;
+      // mean reversion is achieved by mean reverting the space the process
+      // diffuses through
       double dxi = dxm * exp(-meanRev*s.term);
       double dui = dum * exp( meanRev*s.term);
-      //cout<<i<<", "<<s.term<<", "<<s.dt<<endl;
       int j;
       for (j=0; j<N; j++){
         s.x[j] = (j-n)*dxi;
@@ -130,45 +147,99 @@ public:
 
   }
 
+  // fit the mesh to the rate curve
   void fit(ShortRateConv conv){
     fill(step[0].ad.begin(),step[0].ad.end(),0.0);
-    //fill(step[0].p.begin(),step[0].p.end(),0.0);
     int n = N/2;
+    // start in middle of first step with PV of $1
     step[0].ad[n]=1;
-    step[0].p[n]=1;
     vector<double> tmp(N);
-    vector<complex<double>> ctmp(NC);
+    vector<complex<double>> ctmp(N);
     for (size_t i=0; i<step.size()-1; i++){
+      // optimize out the fitting term gamma
       fitStep(i, conv);
       // with gamma found set up rates arrays an diffuse to next step
       Step &s = step[i];
       for (int j=0; j<N; j++){
         s.r[j] = conv(s.x[j], s.gamma);
-        s.fdf[j] = exp(-s.dt * s.r[i]);
+        s.fdf[j] = exp(-s.dt * s.r[j]);
         tmp[j] = s.ad[j] * s.fdf[j];
+        //cout<<i<<", "<<j<<", "<<s.r[j]<<", "<<s.ad[j]<<", "<<s.fdf[j]<<", "<<s.x[j]<<", "<<s.dt<<endl;
       }
-      rfft_forward(pfft, tmp.data(), ctmp.data());
+      // perform convolution to get state prices for next step
+      rfft_forward(pfft,tmp.data(),ctmp.data());
       for (int j=0; j<NC; j++){
-        ctmp[j] *= conj(s.phi[j]);
+        ctmp[j] *= conj(phi(s.u[j],s.dt));
       }
       rfft_inverse(pfft, ctmp.data(), step[i+1].ad.data());
-      //break;
+
     }
   }
 
+  // use Brent's method to fit short rates to next step
   void fitStep(int i, ShortRateConv conv){
     double B = step[i+1].bond;
     Step &s = step[i];
     double prevGamma = 0;
     if (i>0) prevGamma = step[i-1].gamma;
-    s.gamma = Brent().solve([&](double g){
+    auto f = [&](double g){
       double value=0;
       for (int j=0; j<N; j++){
         value += s.ad[j] * exp(-s.dt * conv(s.x[j],g));
       }
       //cout<< g<<", "<<value<<", "<<B<<endl;
       return value - B;
-    }, 1e-14, prevGamma, 1.0);
+    };
+    s.gamma = Brent().solve(f, 1e-14, prevGamma, 0.5);
+  }
+
+  void printSteps(){
+    for (size_t i=0; i<step.size(); i++){
+      step[i].printDebug(i);
+    }
+  }
+
+  void clearValues(){
+    for (size_t i=0; i<step.size(); i++){
+      fill(step[i].value.begin(),step[i].value.end(),0.0);
+    }
+  }
+
+  // diffuse and discount values array from step i to i-1
+  void stepBack(size_t i){
+    vector<complex<double>> ctmp(NC,0.0);
+    int j;
+    rfft_forward(pfft,step[i].value.data(),ctmp.data());
+    for (j=0; j<NC; j++){
+      ctmp[j] *= phi(step[i-1].u[j],step[i-1].dt);
+    }
+    rfft_inverse(pfft,ctmp.data(),step[i-1].value.data());
+    for (j=0; j<N; j++){
+      step[i-1].value[j] *= step[i-1].fdf[j];
+    }
+  }
+
+  /*
+  A simple callable bond implementation. Many security types can be valued
+  in the same backward induction manner.
+  */
+  double priceCallableBond(double exercisePrice){
+    clearValues();
+    for (size_t i=step.size()-1; i>0; i--){
+      Step &s = step[i];
+      double price = exercisePrice + s.accrued;
+      for (int j=0; j<N; j++){
+        if (s.canExercise){
+          if (s.value[j] > price){
+            s.value[j] = price;
+          }
+        }
+        s.value[j] += s.cashFlow;
+      }
+      stepBack(i);
+    }
+    // the PV sits where we started building, the middle spot of step 0
+    return step[0].value[N/2];
   }
 };
 
@@ -177,39 +248,96 @@ complex<double> normalCharacteristcFunction(double sigma,double u,double t){
   return exp(-0.5*sigma*sigma*u*u*t);
 }
 
-// short rate conversion func
+// see Hainaut and MacGilchrist (2010) for an attempt at
+// using this process with a pentanomial tree
+struct NormalInverseGaussian{
+  double alpha,beta,delta,gamma;
+  NormalInverseGaussian(double a,double b,double d){
+    alpha=a;
+    beta=b;
+    delta=d;
+    gamma=sqrt(alpha*alpha-beta*beta);
+  }
+  // cf of NIG process (equation 2.2 of H&M paper)
+  complex<double> operator()(double u,double dt){
+    complex<double> I(0,1);
+    complex<double> a = gamma - sqrt(alpha*alpha-pow(beta+I*u,2));
+    return exp(delta*a*dt);
+  }
+};
+
+
+// short rate conversion functions
 double exponentialLevy(double x,double gamma){
   return exp(x+gamma);
 }
+double linearLevy(double x,double gamma){
+  return x+gamma;
+}
+double shiftedExponentialLevy(double shift,double x,double gamma){
+  return exp(x+gamma)-shift;
+}
 
+/*
+A
+*/
 void testCallableBond(){
   // Bond setup
   Date valuation(10,Jan,2017);
   Date maturity(15,Nov,2030);
   Date issue(15,Nov,2010);
   double notional = 10000;
-  double coupon = 3.5; // in %
+  double coupon = 3; // in %
   Frequency payFreq = Semiannual;
-  Calendar calendar = Canada();
+  Calendar calendar = UnitedStates();
   DayCounter dayCount = Thirty360();
   BusinessDayConvention roll = ModifiedFollowing;
 
   // Option setup
-  int nstep = 200;
+  int nstep = 360;
   int Nfft = 2048;
-  double sigma = 0.275;
-  double meanReversion = 0.02;
+
+  double meanReversion = 0.01;
   double callPenalty = 1.02; // early exercise penalty
-  // normal dist + exponential short rate =  Black-Karasiski model
-  CharacteristcFunction cf = bind(normalCharacteristcFunction,
-    sigma, placeholders::_1, placeholders::_2);
-  ShortRateConv shortRate = exponentialLevy;
+  int model = 1; // chose from stochastic models
+  CharacteristcFunction cf;
+  ShortRateConv shortRate;
+  if (model==0){
+    // normal dist + exponential short rate =  Black-Karasiski model
+    double sigma = 0.275;
+    cf = bind(normalCharacteristcFunction,
+      sigma, placeholders::_1, placeholders::_2);
+    shortRate = exponentialLevy;
+  }else if (model==1){
+    // normal dist + linear short rate =  Hull-White model
+    double sigma = 0.01;
+    cf = bind(normalCharacteristcFunction,
+      sigma, placeholders::_1, placeholders::_2);
+    shortRate = linearLevy;
+  }else if (model==2){
+    // normal dist + shifted exponential short rate = shifted Black-Karasiski model
+    // increasingly popular model for EU fixed income options
+    double sigma = 0.10;
+    double shift = 0.04;
+    cf = bind(normalCharacteristcFunction,
+      sigma, placeholders::_1, placeholders::_2);
+    shortRate = bind(shiftedExponentialLevy,
+      shift, placeholders::_1, placeholders::_2);
+  }else if (model==3){
+    // NIG with linear scale is Hainaut and MacGilchrist's model.
+    // I have yet to work up the motivation to build a pentanomial tree
+    // and good implementations of this are scarce so I will only
+    // claim that it 'should' replicate.
+    cf = NormalInverseGaussian(100.14,5.52,6.361e-5);
+    shortRate = linearLevy;
+  }
+
   // true to only allow exercise on cf dates, false for american exercise
-  bool isBermudan = true;
+  bool isBermudan = false;
 
   // Rate curve setup with made up numbers
-  vector<int> rterms = {1,2,5,10,20,30};
-  vector<double> rates = {0.02,0.0225,0.025,0.03,0.032,0.034};
+  vector<int> rterms = {0, 1,2,5,10,20,30};
+  vector<double> rates = {0.018, 0.02,0.0225,0.025,0.03,0.032,0.034};
   vector<Date> rdates;
   for (size_t i=0; i<rterms.size(); i++){
     Date d = valuation + Period(rterms[i],Years);
@@ -224,13 +352,8 @@ void testCallableBond(){
     .withConvention(roll).withTerminationDateConvention(roll)
     .backwards();
 
-  vector<double> coup = {0};
   vector<Date> dates = sched.dates();
-  for (size_t i=1; i<dates.size(); i++){
-    double dt = dayCount.yearFraction(dates[i-1],dates[i]);
-    coup.push_back( dt * (coupon/100) * notional );
-    //cout<<i<<": "<<dates[i]<<", "<<coup.back()<<endl;
-  }
+
   vector<double> reqTimes = {0};
   for (size_t i=1; i<dates.size(); i++){
     if (dates[i]>valuation)
@@ -246,7 +369,48 @@ void testCallableBond(){
   }
   mesh.fit(shortRate);
   //setup bond cashflows and accrued interest
-  //for 
+  for (size_t i=0; i<sched.size(); i++){
+    if (sched[i] <= valuation) continue;
+    double term = dayCount.yearFraction(valuation, sched[i]);
+    size_t j = tg.index(term);
+    mesh.step[j].cashFlow = (coupon/100.0/(int)payFreq) * notional;
+
+    mesh.step[j].canExercise = true;
+    if (i>0){
+      double pterm = dayCount.yearFraction(valuation, sched[i-1]);
+      size_t pj = tg.closestIndex(pterm);
+      for (size_t k=(pj>0?pj+1:0); k<j; k++){
+        mesh.step[k].accrued = (mesh.step[k].term - pterm)/(term-pterm);
+        mesh.step[k].accrued *= mesh.step[j].cashFlow;
+      }
+    }
+    if (i==sched.size()-1){
+      mesh.step[j].cashFlow += notional;
+      mesh.step[j].canExercise = false;
+    }
+  }
+  if (!isBermudan){
+    // american exercise
+    for (size_t i=0; i<mesh.step.size(); i++)
+      mesh.step[i].canExercise = true;
+  }
+  //mesh.printSteps();
+
+  // price bond normally as sum of CF * discount
+  double bondPV=0;
+  for (size_t i=0; i<mesh.step.size(); i++){
+    Step &s = mesh.step[i];
+    bondPV += s.bond * s.cashFlow;
+  }
+  printf("Bond PV: %.8f\n", bondPV);
+  // test pricing a normal bond by setting an unreachable strike price
+  double value2 = mesh.priceCallableBond(notional * 1e5);
+  printf("Bond PV Check: %.8f\n", value2);
+  // price option bond
+  double value = mesh.priceCallableBond(notional * callPenalty);
+  printf("Option Bond PV: %.8f\n", value);
+
+
 }
 
 int main(){
