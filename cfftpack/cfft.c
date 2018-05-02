6a178982d4138be66b42cdaac7322e3cde187c0c
Roy Zywina <roy@zywina.ca>
2018-05-02 00:22:59 -0400

    makefiles

--- cfftpack/cfft.c
+++ cfftpack/cfft.c
@@ -1,1 +1,25 @@
 
+#include <stdlib.h>
+#include "cfft.h"
+#include <tgmath.h>
+
+#if defined(__STDC_VERSION__) && __STDC_VERSION__>=201112L
+  #define fft_malloc(x) alligned_alloc(16,(x))
+#else
+  #define fft_malloc(x) malloc(x)
+#endif
+
+int fft_work_simulate(fft_work_t *w){
+
+}
+
+fft_work_t *fft_work_create(int N){
+  fft_work_t *w = malloc(sizeof(fft_work_t));
+  w->size = N;
+  w->twiddle = fft_malloc(N*2);
+  return w;
+}
+
+void fft_work_free(fft_work_t *f){
+  free(f);
+}
--- cfftpack/cfft.h
+++ cfftpack/cfft.h
@@ -9,5 +9,20 @@ Roy Zywina, Feb 2018
 
 #include "fftpack.h"
 
+typedef struct{
+  int butterfly;
+  int start,skip,size;
+} fft_control_t;
+
+typedef struct{
+  fft_complex_t *data;
+  fft_real_t *twiddle;
+  int size;
+} fft_work_t;
+
+
+fft_work_t *fft_work_create(int N);
+void fft_work_free(fft_work_t *f);
+
 
 #endif
