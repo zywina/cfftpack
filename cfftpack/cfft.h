11fba218d229837da942f24c007f4097aa08c663
Roy Zywina <roy@zywina.ca>
2018-05-02 00:21:54 -0400

    experimental work

---
+++ cfftpack/cfft.h
@@ -0,0 +1,13 @@
+/*
+Rebuilding internal algorithms, split radix support, precache work
+for faster runtimes, etc.
+
+Roy Zywina, Feb 2018
+*/
+#ifndef _CFFT_H_
+#define _CFFT_H_
+
+#include "fftpack.h"
+
+
+#endif
